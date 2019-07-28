d10c9d4e438d9d74192369fdc0751be6ebe0ee27
ravipun <ravipuj8@gmail.com>
2019-07-28 11:03:19 +0700

    first Initiate

---
+++ public/js/app.js
@@ -0,0 +1,37 @@
+console.log('Client side javascript file is loaded!')
+
+// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
+//     response.json().then((data)=>{
+//         console.log(data)
+//     })
+// })
+
+
+
+
+const weatherFrom = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne  = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+//messageOne.textContent = 'From Javascript'
+
+weatherFrom.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const location = search.value
+    messageOne.textContent = 'Loading.......'
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error) {
+            messageOne.textContent = data.error
+        } else {
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+        }
+    })
+})
+
+
+
+})
